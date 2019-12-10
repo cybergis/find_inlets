38259eb87acf5dfd9de377ac3be65cf1a6e5ea90
Zhiyu(Drew) Li <zyli2004@gmail.com>
2019-12-10 14:18:38 -0600

    change GDAL_HOME

--- compile-on-ubuntu.sh
+++ compile-on-ubuntu.sh
@@ -2,14 +2,15 @@
 # apt install build-essential cmake
 # apt install libgdal
 
-export CPLUS_INCLUDE_PATH=/usr/include/gdal
-export C_INCLUDE_PATH=/usr/include/gdal
+#export CPLUS_INCLUDE_PATH=/usr/include/gdal
+#export C_INCLUDE_PATH=/usr/include/gdal
+export GDAL_HOME=/usr/include/gdal
 
 echo "GDAL_HOME=$GDAL_HOME"
 bdir=`dirname $0`/build
 [ -d $bdir ] && echo "Deleting $bdir ..." && rm -fr $bdir
 mkdir build
 cd build
-cmake .. -DGDAL_INCLUDE_DIR=${GDAL_HOME}/include
+cmake .. -DGDAL_INCLUDE_DIR=${GDAL_HOME}
 make
 make install
