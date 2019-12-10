0cef08a21b3ab90eea74e3fd48b0626ebfbed0bc
Zhiyu(Drew) Li <zyli2004@gmail.com>
2019-12-10 14:35:41 -0600

    update

--- compile-on-ubuntu.sh
+++ compile-on-ubuntu.sh
@@ -7,13 +7,13 @@
 export GDAL_HOME=/usr/include/gdal
 #export GDAL_LIBRARY=/usr/lib
 #export GDAL_INCLUDE_DIR=/usr/include/gdal
-export GDAL_DIR=/usr
+#export GDAL_DIR=/usr
 
 echo "GDAL_HOME=$GDAL_HOME"
 bdir=`dirname $0`/build
 [ -d $bdir ] && echo "Deleting $bdir ..." && rm -fr $bdir
 mkdir build
 cd build
-cmake .. -DGDAL_INCLUDE_DIR=${GDAL_HOME}
+cmake .. -DGDAL_INCLUDE_DIR=${GDAL_HOME} -DGDAL_LIBRARY=/usr/lib
 make
 make install
