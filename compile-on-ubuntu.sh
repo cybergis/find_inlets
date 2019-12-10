d01acb567471dc36d72a57881ae4ee05146383ad
Zhiyu(Drew) Li <zyli2004@gmail.com>
2019-12-10 16:38:01 -0600

    update

--- compile-on-ubuntu.sh
+++ compile-on-ubuntu.sh
@@ -1,19 +1,11 @@
 # Tested on Ubuntu 18.04
 # apt install build-essential cmake
-# apt install libgdal
+# apt install libgdal-dev
 
-#export CPLUS_INCLUDE_PATH=/usr/include/gdal
-#export C_INCLUDE_PATH=/usr/include/gdal
-export GDAL_HOME=/usr/include/gdal
-#export GDAL_LIBRARY=/usr/lib
-#export GDAL_INCLUDE_DIR=/usr/include/gdal
-#export GDAL_DIR=/usr
-
-echo "GDAL_HOME=$GDAL_HOME"
 bdir=`dirname $0`/build
 [ -d $bdir ] && echo "Deleting $bdir ..." && rm -fr $bdir
 mkdir build
 cd build
-cmake .. -DGDAL_INCLUDE_DIR=${GDAL_HOME} -DGDAL_LIBRARY=/usr/lib
+cmake ..
 make
 make install
