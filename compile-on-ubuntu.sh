0bd734bfe152d7289b0b95e9414247d2e23f098a
Zhiyu(Drew) Li <zyli2004@gmail.com>
2019-12-10 16:52:44 -0600

    update

--- compile-on-ubuntu.sh
+++ compile-on-ubuntu.sh
@@ -2,6 +2,9 @@
 # apt install build-essential cmake
 # apt install libgdal-dev
 
+export CPLUS_INCLUDE_PATH=/usr/include/gdal
+export C_INCLUDE_PATH=/usr/include/gdal
+
 bdir=`dirname $0`/build
 [ -d $bdir ] && echo "Deleting $bdir ..." && rm -fr $bdir
 mkdir build
