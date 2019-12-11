f8d0d876479c5cbaa19f2ef373a20c5b55955722
zhiyuli <zyli2004@gmail.com>
2019-12-11 02:25:47 +0000

    compile on ub

--- compile-on-ubuntu.sh
+++ compile-on-ubuntu.sh
@@ -1,14 +1,17 @@
 # Tested on Ubuntu 18.04
-# apt install build-essential cmake
+# apt install build-essential
 # apt install libgdal-dev
 
-export CPLUS_INCLUDE_PATH=/usr/include/gdal
-export C_INCLUDE_PATH=/usr/include/gdal
+flags=$(/usr/bin/gdal-config --cflags)
+echo $flags
+libs=$(/usr/bin/gdal-config --libs)
+echo $libs
 
 bdir=`dirname $0`/build
 [ -d $bdir ] && echo "Deleting $bdir ..." && rm -fr $bdir
 mkdir build
-cd build
-cmake .. -DGDAL_INCLUDE_DIR=/usr/include/gdal
-make
-make install
+cd src
+g++ find_inlets.cpp -o ../build/find_inlets_mr $flags $libs
+g++ find_inlets_hr.cpp -o ../build/find_inlets_hr $flags $libs
+ls ../build
+cp ../build/* /usr/bin/
